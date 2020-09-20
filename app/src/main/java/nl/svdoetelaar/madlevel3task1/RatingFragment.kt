ac2a49b982125b880da2d15f5f3084305af97081
Sander <sandervandenoetelaar@hotmail.com>
2020-09-20 13:15:47 +0200

    4.2

--- app/src/main/java/nl/svdoetelaar/madlevel3task1/RatingFragment.kt
+++ app/src/main/java/nl/svdoetelaar/madlevel3task1/RatingFragment.kt
@@ -7,6 +7,7 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
 import androidx.navigation.fragment.findNavController
+import kotlinx.android.synthetic.main.rating_fragment.*
 
 /**
  * A simple [Fragment] subclass as the second destination in the navigation.
@@ -14,8 +15,8 @@ import androidx.navigation.fragment.findNavController
 class RatingFragment : Fragment() {
 
     override fun onCreateView(
-            inflater: LayoutInflater, container: ViewGroup?,
-            savedInstanceState: Bundle?
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.rating_fragment, container, false)
@@ -24,8 +25,26 @@ class RatingFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        view.findViewById<Button>(R.id.button_second).setOnClickListener {
-            findNavController().navigate(R.id.action_SecondFragment_to_FirstFragment)
+        view.findViewById<Button>(R.id.btnToSummary).setOnClickListener {
+            findNavController().navigate(R.id.action_ratingFragment_to_startFragment)
         }
+
+        showRandomAssessableGame()
+    }
+
+    private fun showRandomAssessableGame() {
+        val randomGame = listOf(
+            "Horizon zero dawn",
+            "Minecraft dungeons",
+            "Bleeding edge",
+            "Half-Life: Alyx",
+            "Doom Eternal",
+            "Call of Duty: Warzone",
+            "HALO: Combat Evolved Anniversary",
+            "Temtem",
+            "Monster Hunter World: Iceborn"
+        ).random()
+
+        tvNameGame.text = randomGame
     }
 }
